cb7f89b0b8dda66174706f73c9071146a754a6c0
zhangjt <862517061@qq.com>
2019-06-11 22:55:40 +0800

    登陆注册页面编写

--- gzhipin-client/src/containers/login/login.jsx
+++ gzhipin-client/src/containers/login/login.jsx
@@ -1,8 +1,52 @@
-import React,{Component} from 'react';
-export default class Login extends Component{
-    render(){
-        return (
-            <div>Login</div>
-        )
-    }
+import React, { Component } from 'react';
+import {
+  NavBar,
+  WingBlank,
+  List,
+  InputItem,
+  WhiteSpace,
+  Button
+} from 'antd-mobile';
+import Logo from '../../components/logo/logo';
+export default class Login extends Component {
+  state = {
+    username: '',  //用户名
+    password: '',  //密码
+  }
+
+  login = () => {
+    console.log(this.state);
+  }
+
+  //处理输入数据的改变，更新对应的状态
+  handleChange = (name, val) => {
+    this.setState({
+      [name]: val
+    })
+  }
+
+  toRegister = () => {
+    this.props.history.replace('/register');
+  }
+
+  render() {
+    return (
+      <div>
+        <NavBar>硅&nbsp;谷&nbsp;直&nbsp;聘</NavBar>
+        <Logo />
+        <WingBlank>
+          <List>
+            <WhiteSpace />
+            <InputItem placeholder="请输入用户名" onChange={val => { this.handleChange('username', val) }}>用户名:</InputItem>
+            <WhiteSpace />
+            <InputItem placeholder="请输入密码" type="password" onChange={val => { this.handleChange('password', val) }}>密&nbsp;&nbsp;&nbsp;码:</InputItem>
+            <WhiteSpace />
+            <Button type="primary" onClick={this.login}>登&nbsp;&nbsp;&nbsp;陆</Button>
+            <WhiteSpace />
+            <Button onClick={this.toRegister}>还没有账户</Button>
+          </List>
+        </WingBlank>
+      </div>
+    )
+  }
 }
--- gzhipin-client/src/containers/register/register.jsx
+++ gzhipin-client/src/containers/register/register.jsx
@@ -1,8 +1,67 @@
-import React,{Component} from 'react';
-export default class Rehister extends Component{
-    render(){
-        return (
-            <div>Register</div>
-        )
-    }
+import React, { Component } from 'react';
+import {
+  NavBar,
+  WingBlank,
+  List,
+  InputItem,
+  WhiteSpace,
+  Radio,
+  Button
+} from 'antd-mobile';
+import Logo from '../../components/logo/logo';
+
+const ListItem = List.Item;
+export default class Rehister extends Component {
+  state = {
+    username: '',  //用户名
+    password: '',  //密码
+    password2: '', //确认密码
+    type: 'dashen',      //用户类型：大神、老板
+  }
+
+  //注册点击事件
+  reister = () => {
+    //console.log(this.state);
+  }
+
+  //处理输入数据的改变，更新对应的状态
+  handleChange = (name, val) => {
+    this.setState({
+      [name]: val
+    })
+  }
+
+  toLogin = () => {
+    this.props.history.replace('/login');
+  }
+  render() {
+    const {type} = this.state;
+    return (
+      <div>
+        <NavBar>硅&nbsp;谷&nbsp;直&nbsp;聘</NavBar>
+        <Logo />
+        <WingBlank>
+          <List>
+            <WhiteSpace />
+            <InputItem placeholder="请输入用户名" onChange={val => { this.handleChange('username', val) }}>用户名:</InputItem>
+            <WhiteSpace />
+            <InputItem placeholder="请输入密码" type="password" onChange={val => { this.handleChange('password', val) }}>密&nbsp;&nbsp;&nbsp;码:</InputItem>
+            <WhiteSpace />
+            <InputItem placeholder="请输入确认密码" type="password" onChange={val => { this.handleChange('password2', val) }}>确认密码:</InputItem>
+            <ListItem>
+              <span>用户类型:</span>
+              &nbsp;&nbsp;&nbsp;
+              <Radio checked={type==="dashen"} onChange={() => this.handleChange('type','dashen')}>大神</Radio>
+              &nbsp;&nbsp;&nbsp;
+              <Radio checked={type==="laoban"} onChange={() => this.handleChange('type','laoban')}>老板</Radio>
+            </ListItem>
+            <WhiteSpace />
+            <Button type="primary" onClick={this.reister}>注&nbsp;&nbsp;&nbsp;册</Button>
+            <WhiteSpace />
+            <Button onClick={this.toLogin}>已有账户</Button>
+          </List>
+        </WingBlank>
+      </div>
+    )
+  }
 }
