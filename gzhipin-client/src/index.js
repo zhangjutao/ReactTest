1e9b38d6af221beaeb771e8e5cbd09fccb33eb35
张举涛 <zhangjt@gooalgene.com>
2019-06-11 17:42:11 +0800

    redux相关插件安装

--- gzhipin-client/src/index.js
+++ gzhipin-client/src/index.js
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
 import {HashRouter, Route, Switch} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './redux/store';
 
 import Register from './containers/register/register'
 import Login from './containers/login/login'
 import Main from './containers/main/main'
 
 ReactDOM.render((
+  <Provider store={store}>
     <HashRouter>
-        <Switch>
-            <Route path='/register' component={Register}></Route>
-            <Route path='/login' component={Login}></Route>
-            <Route component={Main}></Route>
-        </Switch>
+      <Switch>
+        <Route path='/register' component={Register}></Route>
+        <Route path='/login' component={Login}></Route>
+        <Route component={Main}></Route>
+      </Switch>
     </HashRouter>
+  </Provider>
+   
 ), document.getElementById('root'));
